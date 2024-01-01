5b5ba2133dff1d2a6c89f618475fed43210b2437
Chris Erickson <chriserickson@live.com>
2024-01-01 11:33:49 -0800

    Update mtg-goldfish-lists.py

--- mtg-goldfish-lists.py
+++ mtg-goldfish-lists.py
@@ -502,7 +502,7 @@ def get_lists():
 # Wait time between format scrapes (seconds). Prevents throttling.
 wait_time = 300
 
-months = ["2023-11"]
+months = ["2023-12"]
 formats = ["legacy","modern","pauper","pioneer","standard","vintage"]
 
 save_multiple_months(months,formats)
