a8061e1f2a04ed5772fe6909200e915d0986bd15
Chris Erickson <chriserickson@live.com>
2025-08-10 19:46:24 -0700

    Update zip-mtgo-logs-build.bat

--- zip-mtgo-logs-build.bat
+++ zip-mtgo-logs-build.bat
@@ -1,3 +1,9 @@
-pyinstaller -w --onefile -i "icon_1.png" zip-mtgo-logs.py
+python -m PyInstaller --console --onefile -i "icon_1.png" zip-mtgo-logs.py
+
+REM Move the built EXE from dist to the project root
+if exist "dist\\zip-mtgo-logs.exe" move /y "dist\\zip-mtgo-logs.exe" ".\\zip-mtgo-logs.exe"
+
+REM Clean up build artifacts
 rmdir build /s /q
 del zip-mtgo-logs.spec
+rmdir dist /s /q
